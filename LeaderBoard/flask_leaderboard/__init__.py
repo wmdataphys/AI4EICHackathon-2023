5c3e50f81d686cb9ee6cfc20a35bd0a945dbe441
karthik18495 <karthik18495@gmail.com>
2023-11-21 16:51:48 -0500

    updated backend

--- LeaderBoard/flask_leaderboard/__init__.py
+++ LeaderBoard/flask_leaderboard/__init__.py
@@ -4,11 +4,28 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_leaderboard.OpenAIAg.OpenAIChat import OpenAIChat
+from flask_leaderboard.utils import OPENAI_Utils
 
+#from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'submissions')
 RES_FOLDER = os.path.join(os.getcwd(), 'actual_results')
+constants = OPENAI_Utils()
+app.config["OPENAI_params"] = constants
+# set default button sytle and size, will be overwritten by macro parameters
+app.config['BOOTSTRAP_BTN_STYLE'] = 'primary'
+app.config['BOOTSTRAP_BTN_SIZE'] = 'sm'
+
+# set default icon title of table actions
+app.config['BOOTSTRAP_TABLE_VIEW_TITLE'] = 'Read'
+app.config['BOOTSTRAP_TABLE_EDIT_TITLE'] = 'Update'
+app.config['BOOTSTRAP_TABLE_DELETE_TITLE'] = 'Remove'
+app.config['BOOTSTRAP_TABLE_NEW_TITLE'] = 'Create'
+app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = "darkly"
+
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['RES_FOLDER'] = RES_FOLDER
 app.config['Q_KEYS'] = dict()
@@ -17,13 +34,18 @@ for k in range(1, 4):
     app.config['Q_KEYS'][k] = os.path.join(app.config['RES_FOLDER'], f'Question{k}_test_labels.csv')
 app.config['MAX_CONTENT_LENGTH'] =  3 * 1000 * 1000 # max 2mb file
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["OPENAI_USERS"] = dict()
+app.config["MAX_TOKENS"] = constants.MAX_TOKENS
+app.config["GPT_MODEL"] = constants.GPT_MODEL
+app.config["TEMPERATURE"] = constants.TEMPERATURE
+
 app.app_context().push()
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+#bootstrap = Bootstrap5(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
@@ -33,5 +55,3 @@ if(not os.path.exists(app.config['RES_FOLDER'])):
 
 if(not os.path.exists(app.config['UPLOAD_FOLDER'])):
     os.makedirs(app.config['UPLOAD_FOLDER'])
-
-from flask_leaderboard import routes
--- LeaderBoard/flask_leaderboard/models.py
+++ LeaderBoard/flask_leaderboard/models.py
@@ -8,13 +8,26 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class Team(db.Model, UserMixin):
+    __tablename__ = 'team'
     id = db.Column(db.Integer, primary_key = True)
     # declaring name only 30 characters max.
     # Caps and small no special characters
-    name = db.Column(db.String(30), nullable = False, unique = True)
-    users = db.relationship('User', backref = 'team_name', lazy = True)
-    questions = db.relationship('Question', backref = 'qteam_name', lazy = True)
-    password = db.Column(db.String(60), unique = False, nullable = False)
+    name = db.Column(db.String(30), 
+                     nullable = False, 
+                     unique = True
+                     )
+    users = db.relationship('User', 
+                            backref = 'TEAM_NAME', 
+                            lazy = True
+                            )
+    questions = db.relationship('Question', 
+                                backref = 'Q_TEAM_NAME', 
+                                lazy = True
+                                )
+    password = db.Column(db.String(60), 
+                         unique = False, 
+                         nullable = False
+                         )
     q1_bestscore = db.Column(db.Float, nullable = True, default = 0.0)
     q2_bestscore = db.Column(db.Float, nullable = True, default = 0.0)
     q3_bestscore = db.Column(db.Float, nullable = True, default = 0.0)
@@ -23,33 +36,76 @@ class Team(db.Model, UserMixin):
         return f"Team('{self.name}')"
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=False, nullable=False)
-    teamname = db.Column(db.String(30), db.ForeignKey('team.name'), nullable = False)
-    password = db.Column(db.String(60), unique = False, nullable = False)
-    OPENAI_API_KEY = db.Column(db.String(60), unique = False, nullable = False)
-    ChatHistory = db.relationship('ChatDB', backref = 'user', lazy = True)
+    username = db.Column(db.String(20), 
+                         unique=False, 
+                         nullable=False
+                         )
+    teamname = db.Column(db.String(30), 
+                         db.ForeignKey('team.name'), 
+                         unique = False, 
+                         nullable = False
+                         )
+    password = db.Column(db.String(60), 
+                         unique = False, 
+                         nullable = False
+                         )
+    OPENAI_API_KEY = db.Column(db.String(60), 
+                               unique = False, 
+                               nullable = False
+                               )
+    questions = db.relationship('Question',
+                                backref = 'Q_USER_NAME',
+                                lazy = True
+                                )
+    AllChatSessions = db.relationship('ChatSessions', 
+                                  backref = 'USER_NAME', 
+                                  lazy = True
+                                  )
     def __repr__(self):
         return f"User('{self.username}')"
 
 class Question(db.Model):
+    __tablename__ = 'question'
     id = db.Column(db.Integer, primary_key = True)
-    teamname = db.Column(db.String(30), db.ForeignKey('team.name'), nullable = False)
-    username = db.Column(db.String(60), nullable = False)
+    teamname = db.Column(db.String(30), 
+                         db.ForeignKey('team.name'), 
+                         nullable = False
+                         )
+    username = db.Column(db.String(60), 
+                         db.ForeignKey('user.username'),
+                         nullable = False
+                         )
     qnumber = db.Column(db.Integer, nullable = False)
     qscore = db.Column(db.Float, unique = False, nullable = False)
     filename = db.Column(db.String(200), nullable = False)
-    submit_time = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    submit_time = db.Column(db.DateTime, 
+                            nullable = False, 
+                            default = datetime.utcnow
+                            )
     remarks = db.Column(db.String(300), nullable = False, default = "")
 
     def __repr__(self):
         return f"Question('{self.teamname}', '{self.username}', {self.qnumber}, {self.qscore}, {self.submit_time})"
 
-class ChatDB(db.Model, UserMixin):
-    __tablename__ = 'chatdb'
+class ChatSessions(db.Model, UserMixin):
+    __tablename__ = 'chatsessions'
     id = db.Column(db.Integer, primary_key = True)
+    sessioname = db.Column(db.String(60), nullable = False)
     username = db.Column(db.String(60), db.ForeignKey('user.username'), nullable = False)
     session_id = db.Column(db.Integer, nullable = False)
+    const_sys_context = db.Column(db.Text(), nullable = False, default = "")
+    user_sys_context = db.Column(db.Text(), nullable = False, default = "")
+    #ChatHistory = db.relationship('ChatInfo', backref = "SESSION_ID", lazy = True)
+    def __repr__(self):
+        return f"ChatSessions('{self.username}', '{self.session_id}')"
+
+class ChatInfo(db.Model, UserMixin):
+    __tablename__ = 'chatinfo'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(60), nullable = False)
+    session_id = db.Column(db.Integer, db.ForeignKey('chatsessions.session_id'), nullable = False)
     user_prompt = db.Column(db.Text(), nullable = False)
     ai_response = db.Column(db.Text(), nullable = False)
     system_response = db.Column(db.Text(), nullable = False)
@@ -57,4 +113,4 @@ class ChatDB(db.Model, UserMixin):
     prompt_tokens = db.Column(db.Integer, nullable = False)
     completion_tokens = db.Column(db.Integer, nullable = False)
     def __repr__(self):
-        return f"ChatDB('{self.username}')"
+        return f"ChatInfo('{self.username}')"
